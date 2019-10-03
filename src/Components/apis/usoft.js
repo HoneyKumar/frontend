967b3361a7549a152a388a11af5c4ff0ca26a13c
Honey Kumar <honey.kumar@virtual.local>
2019-10-03 18:35:28 +0530

    fetching results from dev 6

--- src/Components/apis/usoft.js
+++ src/Components/apis/usoft.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 const clientName = 'client';
 
 export default axios.create({
-    baseURL : 'https://dev-three.usoft.co.uk/'+clientName+'/api',
+    baseURL : 'https://dev-six.usoft.co.uk/'+clientName+'/api',
 });
